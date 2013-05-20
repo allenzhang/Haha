1752f9a25489cbf4cb78c0660eaa329604ba53f7
allenzhang <hb.zhanglu@gmail.com>
2013-05-20 18:53:07 +0800

    change dir

---
+++ webapps/yjssm/index.php
@@ -0,0 +1,122 @@
+<?php require_once dirname(__FILE__) . '/inc/header.html'; ?>
+
+
+		<div class="row-fluid">
+			<div class="span10 offset1" style="background-color:#eee;border-radius:5px;margin-top:10px;margin-bottom:10px;">
+				<div class="span3" style="width:25%;height:250px;margin: 25px 10px 10px 40px;line-height: 30px;">	
+					<h4 style="text-align:center">衡水永佳电动伸缩门配件厂</h4>公司已有十余年的发展历史，主要生产电动伸缩门配件（轮类，轮罩，机头轮，行走轮等），专业维修电动伸缩门，在市场经济的大潮下，本厂跻身国内大型专业生产厂家的行列，生产的铸铁轮，橡胶轮等产品被许多国家大中型重点工程所采用，取得了经济效益和社会效益的双丰收。
+					<p style="text-align:right;color:orange"><a href="#">更多信息&gt;&gt;</a></p>
+				</div>
+
+				<div style="width:67%;float:left;display:block;margin:20px 10px 5px 10px">
+				<div id="myCarousel" class="carousel slide" >
+					<ol class="carousel-indicators">
+						<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+						<li data-target="#myCarousel" data-slide-to="1"></li>
+						<li data-target="#myCarousel" data-slide-to="2"></li>
+					</ol>
+					<div class="carousel-inner">
+						<div class="active item"><img style="width:700px;height:300px" src="http://www.bootcss.com/assets/img/bootstrap-mdo-sfmoma-01.jpg" alt=""></div>
+						<div class="item"><img style="width:700px;height:300px" src="http://www.bootcss.com/assets/img/bootstrap-mdo-sfmoma-02.jpg" alt=""></div>
+						<div class="item"><img style="width:700px;height:300px" src="http://www.bootcss.com/assets/img/bootstrap-mdo-sfmoma-03.jpg" alt=""></div>
+					</div>
+					<a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+					<a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+				</div>
+				</div>
+				<script>
+					$('.carousel').carousel();
+				</script>
+			</div>
+		</div>	
+
+		<div class="row-fluid">
+			<ul class="thumbnails">
+				<li class="span2 offset1">
+				<div class="thumbnail" style="background-color: #eee;">
+					<img src="media/img/1.png">
+					<h5 style="text-align:center;"><a href="#" style="color:black">机头轮</a></h5>
+				</div>
+				</li>
+					<li class="span2">
+				<div class="thumbnail" style="background-color: #eee;">
+					<img src="media/img/1.png">
+					<h5 style="text-align:center;"><a href="#" style="color:black">机头轮</a></h5>
+				</div>
+				</li>
+					<li class="span2">
+				<div class="thumbnail" style="background-color: #eee;">
+					<img src="media/img/1.png">
+					<h5 style="text-align:center;"><a href="#" style="color:black">机头轮</a></h5>
+				</div>
+				</li>
+					<li class="span2">
+				<div class="thumbnail" style="background-color: #eee;">
+					<img src="media/img/1.png">
+					<h5 style="text-align:center;"><a href="#" style="color:black">机头轮</a></h5>
+				</div>
+				</li>
+					<li class="span2">
+				<div class="thumbnail" style="background-color: #eee;">
+					<img src="media/img/1.png">
+					<h5 style="text-align:center;"><a href="#" style="color:black">机头轮</a></h5>
+				</div>
+				</li>
+			</ul>
+			<div style="text-align:right"><a href="#">更多产品</a></div>
+		</div>
+
+		<div style="width:83%;height:1px;margin:0px auto;padding:0px;background-color:#D5D5D5;overflow:hidden;"></div>
+		<div class="row-fluid">
+			<div class="span10 offset1" style="margin-top:25px">
+				<div class="span4">
+					<a href="#" class="thumbnail"><img style="width:100%;height:100%" src="media/img/5/1.jpg"></a>
+				</div>
+				<div class="span4">
+					<a href="#" class="thumbnail"><img style="width:100%;height:100%" src="media/img/5/2.jpg"></a>
+				</div>
+				<div class="span4">
+					<a href="#" class="thumbnail"><img style="width:100%;height:100%" src="media/img/5/3.jpg"></a>
+				</div>
+
+			</div>
+		</div>
+		<div class="row-fluid">
+			<div class="span10 offset1" style="margin-top:10px">
+				<div class="span4">
+					<h5>行业咨询</h5>
+					<ul>
+						<li>的说法撒旦法撒法撒反对撒旦法撒旦法</li>
+						<li>凡达斯蒂法撒旦法撒旦法玩儿玩儿微沃</li>
+						<li>的说法撒旦法撒法撒反对撒旦法撒旦法</li>
+						<li>的说法撒旦法撒法撒反对撒旦法撒旦法</li>
+						<li>的说法撒旦法撒法撒反对撒旦法撒旦法</li>
+						<li>凡达斯蒂法撒旦法撒旦法玩儿玩儿微沃</li>
+					</ul>
+					<p style="text-align:center"><a href="#">更多信息&gt;&gt;</a></p>
+
+				</div>
+				<div class="span4">
+					<h5>销售网络</h5>
+					<ul class="unstyled">
+						<li>华东地区: 沈阳 大连 长春 黑龙江 鸡西</li>
+						<li>华北地区：北京 天津 承德 秦皇岛 霸州</li>
+						<li>华东地区：济南 济宁 南京 义乌</li>
+						<li>华中地区：武汉 襄樊 异常 长沙</li>
+						<li>华南地区：广州 深圳 珠海 厦门 海口</li>
+						<li>西部地区: 兰州 西安 重庆 成都</li>
+					</ul>
+					<p style="text-align:center"><a href="#">更多信息&gt;&gt;</a></p>
+				</div>
+				<div class="span4">
+					<h5>在线留言</h5>
+					<textarea style="height:140px;width:96%" rows="5" placeholder="请记得留下您的联系方式"></textarea>
+					<button class="btn" style="float:right;" type="submit">提交</button>
+				</div>
+			</div>
+
+		</div>
+
+
+<?php require_once dirname(__FILE__) . '/inc/footer.html'; ?>
+		
---
+++ webapps/yjssm/news.php
@@ -0,0 +1,46 @@
+<?php require_once dirname(__FILE__) . '/inc/header.html'; ?>
+<div class="row-fluid">
+	<div class="span7 offset1">
+		<div class="row-fluid" style="background-color:#eee">
+			<div class="span4">
+				<a href="#" class="thumbnail"><img src="media/img/min1.png"></a>
+			</div>
+			<div class="span8" style="margin:10px 5px 10px 10px">
+				<h5>台湾或对菲持续制裁 调查真相菲方拒合作</h5>
+				在启动实施11项对菲律宾制裁实施后，台湾方面16日既进行演习“肌肉秀”，也派出调查组赴菲希望合作查案。菲方则认为已做出额外让步，表态不会与台湾方面开展联合调查，因此，调查真想恐一波三折。台湾方面高层提出要继续施压，已经做好对菲持久制裁准备。
+			</div>	
+		</div>
+
+
+		<div class="row-fluid">
+			<div class="span6" style="background-color:#eee">
+			</div>
+			<div class="span6" style="background-color:#eee">
+			</div>
+		</div>
+		<div class="row-fluid">
+			<div class="span6" style="background-color:#eee">
+			</div>
+			<div class="span6" style="background-color:#eee">
+			</div>
+		</div>
+		<div class="row-fluid">
+			<div class="span6" style="background-color:#eee">
+			</div>
+			<div class="span6" style="background-color:#eee">
+			</div>
+		</div>
+
+	</div>
+	<div class="span3">
+		<div class="row-fluid">
+			<div class="span12" style="background-color:#eee">
+			</div>
+			<div class="span12" style="background-color:#eee">
+			</div>
+			<div class="span12" style="background-color:#eee">
+			</div>
+		</div>
+	</div>
+</div>
+<?php require_once dirname(__FILE__) . '/inc/footer.html'; ?>
